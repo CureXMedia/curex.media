422bbd6dbea353ef27916bed88137678cbd1b250
sanket-sapate <sanketsapatevnit@gmail.com>
2024-01-03 19:11:10 +0530

    tailwind done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css';
 function App() {
  
   return (
     <>
-        <div className='p-2 border-2 bg-black border-slate-700'>
+        <div className=''>
+          Hi
         </div>
     </>
   );
