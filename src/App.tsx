77de80995042ab6788aac3e9af0e9b128c4490b0
root <root@localhost.localdomain>
2024-01-03 16:16:27 +0530

    Temlate setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+function App() {
+ 
+  return (
+    <>
+        <div className='p-2 border-2 bg-black border-slate-700'>
+        </div>
+    </>
+  );
+}
+
+export default App;
